bbfb1bc426b782739a0282a876373e16252b5836
settings <juan@fizzmod.com>
2019-07-04 16:40:02 -0300

    tests, readme, v1.0.0

--- README.md
+++ README.md
@@ -1,21 +1,56 @@
 # settings
 
-[![Build Status](https://travis-ci.org/janis-commerce/settings.svg?branch=master)](https://travis-ci.org/janis-commerce/settings)[![Coverage Status](https://coveralls.io/repos/github/janis-commerce/settings/badge.svg?branch=master)](https://coveralls.io/github/janis-commerce/settings?branch=master)
-
-
+[![Build Status](https://travis-ci.org/janis-commerce/settings.svg?branch=master)](https://travis-ci.org/janis-commerce/settings)
+[![Coverage Status](https://coveralls.io/repos/github/janis-commerce/settings/badge.svg?branch=master)](https://coveralls.io/github/janis-commerce/settings?branch=master)
 
 ## Installation
-```sh
+```bash
 npm install janiscommerce/settings
 ```
 
 ## API
 
+### Settings.get(string) **static**
+- This method returns settings or settings by key.
+**Example:** Settings.get();
+**Example:** Settings.get('database');
+
+## Setting file
+
+The setting file is a JSON with all the settings.
+
+### Example
+```json
+{
+	"database": {
+		"core": {
+			"host": "my-host",
+			"user": "the-user"
+		},
+		"otherConn": {
+			"host": "my-other-host",
+			"user": "other-user"
+		}
+	},
+	"otherConfig": 123,
+	"mainPath": "/main/path/"
+}
+```
 
 ## Usage
+
+### How to get settings
+
 ```js
 const Settings = require('janiscommerce/settings');
 
+const settings = Settings.get();
 ```
 
-## Examples
+### How to get settings by key
+
+```js
+const Settings = require('janiscommerce/settings');
+
+const settings = Settings.get('database');
+```
--- tests/settings-test.js
+++ tests/settings-test.js
@@ -1,11 +1,114 @@
 'use strict';
 
 const assert = require('assert');
+const sandbox = require('sinon').createSandbox();
+const mockRequire = require('mock-require');
 
-const { Settings } = require('./../lib');
+const Settings = require('./..');
 
 describe('Settings', () => {
 
-	// your tests here...
+	let spySetCache;
+
+	beforeEach(() => {
+		delete process.env.MS_PATH;
+		spySetCache = sandbox.spy(Settings, '_setCache');
+	});
+
+	afterEach(() => {
+		Settings._settingsCache = undefined; // eslint-disable-line
+		sandbox.restore();
+		mockRequire.stopAll();
+	});
+
+	const mockConfig = config => {
+		mockRequire(Settings.configPath, config);
+	};
+
+	context('when config file not found', () => {
+
+		it('should return an empty object when getting without key', () => {
+			const settings = Settings.get();
+			assert.deepEqual(settings, {});
+		});
+
+		it('should return undefined when getting by key', () => {
+			const settings = Settings.get('my-key');
+			assert.deepEqual(settings, undefined);
+		});
+
+		it('should cache settings', () => {
+			Settings.get();
+			Settings.get();
+
+			sandbox.assert.calledOnce(spySetCache);
+		});
+	});
+
+	context('when invalid config file found', () => {
+
+		it('should return an empty object when getting without key', () => {
+
+			mockConfig(['bad', 'config']);
+
+			const settings = Settings.get();
+			assert.deepEqual(settings, {});
+		});
+
+		it('should return undefined when getting with key', () => {
+
+			mockConfig(['bad', 'config']);
+
+			const settings = Settings.get('my-key');
+
+			assert.deepEqual(settings, undefined);
+		});
+	});
+
+	context('when config file found', () => {
+
+		it('should return the whole config when getting without key', () => {
+
+			const wholeConfig = { configFoo: 'foo', configBar: { bar: 1 } };
+
+			mockConfig(wholeConfig);
+
+			const settings = Settings.get();
+
+			assert.deepEqual(settings, wholeConfig);
+		});
+
+		it('should return undefined when getting by key and not found that key', () => {
+
+			const wholeConfig = { configFoo: 'foo', configBar: { bar: 1 } };
+
+			mockConfig(wholeConfig);
+
+			const settings = Settings.get('my-key');
+
+			assert.deepEqual(settings, undefined);
+		});
+
+		it('should return the settings for a key when found', () => {
+
+			const wholeConfig = { configFoo: 'foo', configBar: { bar: 1 } };
+
+			mockConfig(wholeConfig);
+
+			assert.deepEqual(Settings.get('configFoo'), wholeConfig.configFoo);
+			assert.deepEqual(Settings.get('configBar'), wholeConfig.configBar);
+		});
+	});
+
+	it('should use MS_PATH as prefix when exists', () => {
+		process.env.MS_PATH = 'my-prefix';
+		assert(Settings.configPath.endsWith('/my-prefix/.janiscommercerc.json'));
+	});
+
+	it('should use MS_PATH as prefix when not exists', () => {
+		// settings/ cause is the folder of the package
+		// configPath getter use process.cwd()
+		assert(Settings.configPath.endsWith('/settings/.janiscommercerc.json'));
+	});
 
 });
